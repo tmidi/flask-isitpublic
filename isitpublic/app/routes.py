116c565dbeac75b7c6bdfa3e01785e63427e34fb
tmidi <taleebmidi@gmail.com>
2019-05-28 23:34:59 -0400

    Remove unused imports and format render_template

--- isitpublic/app/routes.py
+++ isitpublic/app/routes.py
@@ -1,12 +1,7 @@
 from flask import render_template, request
-import socket
-import requests
 from forms import IpForm
 import ipaddress
 from functions import is_valid_ipv4_address, is_valid_ipv6_address
-
-
-
 from app import app
 
 
@@ -14,18 +9,28 @@ from app import app
 def index():
     form = IpForm()
     error = ''
-    result= ''
+    result = ''
     if request.method == "POST" and form.validate():
         form = IpForm()
         ip = form.address.data
         if is_valid_ipv4_address(ip) or is_valid_ipv6_address(ip):
             if ipaddress.ip_address(ip).is_global:
-                return render_template("index.html", form=form, result=True,error=error, ip=ip)
+                return render_template("index.html",
+                                       form=form,
+                                       result=True,
+                                       error=error, ip=ip)
             else:
-                return render_template("index.html", form=form, result=False,error=error, ip=ip)
+                return render_template("index.html",
+                                       form=form,
+                                       result=False,
+                                       error=error,
+                                       ip=ip)
         else:
             error = "Not a valid IP Address"
-            return render_template("index.html", form=form, result=result, error=error)
+            return render_template("index.html",
+                                   form=form,
+                                   result=result,
+                                   error=error)
 
     else:
         return render_template("index.html", form=form)
